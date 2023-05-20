a4e0b7066632dc47d49eb79a614947d46cef17c8
ousf <ousf.dev@gmail.com>
2023-05-21 00:43:50 +0100

    remove test controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DimOneController } from './dim-one/dim-one.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, DimOneController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/dim-one/dim-one.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('dim-one')
-export class DimOneController {
-  @Get()
-  getALLDim(): string {
-    return 'All dims';
-  }
-}
